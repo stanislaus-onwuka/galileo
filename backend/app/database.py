56b8e33684990e3c6a81a0dcabc9afa9dab52947
stanislaus-onwuka <kachi2505@yahoo.com>
2024-09-18 07:21:56 +0100

    DB setup in progress

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,6 +2,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_DETAILS = "mongodb://localhost:27017"
 
+MONGO_URI = "mongodb+srv://stanstyler25:HXrD0dmHQLcxoigh@cluster0.3xr8e3c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 client = AsyncIOMotorClient(MONGO_DETAILS)
 database = client.get_database('galileo')
 
